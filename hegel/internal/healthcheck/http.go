823f275b50466160f27c044e7aaea3a690c20b27
Jacob Weinstock <jakobweinstock@gmail.com>
2025-01-31 20:16:52 -0700

    Removing Hegel health checking pkg:
    It's not in use right now. More thought
    needs to be put into how to handle health
    checking holistically across all services.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- hegel/internal/healthcheck/http.go
+++
@@ -1,8 +0,0 @@
-package healthcheck
-
-import "github.com/gin-gonic/gin"
-
-// Configure configures router with a /healthz endpoint using a handler created with NewHandler.
-func Configure(router gin.IRouter, client Client) {
-	router.GET("/healthz", NewHandler(client))
-}
