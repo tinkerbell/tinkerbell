ab582b88c8cd6d3d2270f1163f54a789930a3dc0
Jacob Weinstock <jakobweinstock@gmail.com>
2025-02-03 13:07:37 -0700

    Print the type for bools in CLI help:
    By default the ff library won't print
    the CLI flag type for boolean values that
    are  defaulted to false. I prefer they
    are printed.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- cmd/flag/flag.go
+++ cmd/flag/flag.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 
 	"github.com/peterbourgon/ff/v4"
+	"github.com/peterbourgon/ff/v4/ffval"
 )
 
 // Config defines the configuration for a flag.
@@ -20,10 +21,19 @@ type Set struct {
 // Register registers a flag with the provided flag set.
 // This will panic if the flag is unable to be added to the flag set, like for a duplicate name.
 func (fs *Set) Register(f Config, fv flag.Value) {
+	ph := func() string {
+		// If the flag is a boolean flag add the static placeholder of "BOOL"
+		if _, ok := fv.(*ffval.Bool); ok {
+			return "BOOL"
+		}
+		return ""
+	}()
+
 	if _, err := fs.AddFlag(ff.FlagConfig{
-		LongName: f.Name,
-		Usage:    f.Usage,
-		Value:    fv,
+		LongName:    f.Name,
+		Usage:       f.Usage,
+		Value:       fv,
+		Placeholder: ph,
 	}); err != nil {
 		panic(err)
 	}
