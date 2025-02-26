348474c4ee6ed35f46a9862a2751378f402f6944
Jacob Weinstock <jakobweinstock@gmail.com>
2025-02-26 09:34:32 -0700

    Add embedded etcd and kube-apiserver:
    This gives us a single tinkerbell binary
    that doesnt need any external services.
    It's working but still needs a lot of clean
    up. All CLI options must be env vars right now.
    Here are the bare minimum that need set. The script,
    ./script/certs.sh, will create the certs needed
    for the kube apiserver and the kubeconfig needed
    for all the other Tinkerbell services.
    
    TINKERBELL_BACKEND_KUBE_CONFIG=script/kubeconfig
    TINKERBELL_IPXE_HTTP_SCRIPT_OSIE_URL=http://IP:Port
    TINKERBELL_IPXE_HTTP_SCRIPT_EXTRA_KERNEL_ARGS="tink_worker_image=ghcr.io/tinkerbell/tink-agent"
    ETCD_SERVERS=http://localhost:2379
    SERVICE_ACCOUNT_KEY_FILE=script/certs/service-account-key.pem
    SERVICE_ACCOUNT_SIGNING_KEY_FILE=script/certs/service-account-key.pem
    SERVICE_ACCOUNT_ISSUER=api
    TLS_CERT_FILE=script/certs/server.crt
    TLS_PRIVATE_KEY_FILE=script/certs/server.key
    CLIENT_CA_FILE=script/certs/ca.crt
    LOGGING_FORMAT=json
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

---
+++ apiserver/apiserver.go
@@ -0,0 +1,19 @@
+package apiserver
+
+import (
+	"context"
+	_ "time/tzdata" // for timeZone support in CronJob
+
+	"github.com/go-logr/logr"
+	"github.com/tinkerbell/tinkerbell/apiserver/internal"
+	"k8s.io/component-base/cli"
+	_ "k8s.io/component-base/logs/json/register"          // for JSON log format registration
+	_ "k8s.io/component-base/metrics/prometheus/clientgo" // load all the prometheus client-go plugins
+	_ "k8s.io/component-base/metrics/prometheus/version"  // for version metric registration
+)
+
+// TODO: pass in logr.Logger
+func Start(ctx context.Context, log logr.Logger) int {
+	command := internal.NewAPIServerCommand(ctx, log)
+	return cli.Run(command)
+}
