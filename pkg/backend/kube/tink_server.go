2f86fba84adc9e567278c2bc8b680e0a191a33ac
Jacob Weinstock <jakobweinstock@gmail.com>
2025-04-03 16:39:41 -0600

    Remove backend Workflow read in enrollment:
    This cuts down on the backend calls when the
    enrollment logic is run.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- pkg/backend/kube/tink_server.go
+++ pkg/backend/kube/tink_server.go
@@ -7,14 +7,11 @@ import (
 
 	v1alpha1 "github.com/tinkerbell/tinkerbell/pkg/api/v1alpha1/tinkerbell"
 	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func (b *Backend) ReadAll(ctx context.Context, workerID string) ([]v1alpha1.Workflow, error) {
 	stored := &v1alpha1.WorkflowList{}
-	err := b.cluster.GetClient().List(ctx, stored, &client.MatchingFields{
-		WorkflowByNonTerminalState: workerID,
-	})
+	err := b.cluster.GetClient().List(ctx, stored /*&client.MatchingFields{WorkflowByNonTerminalState: workerID}*/)
 	if err != nil {
 		return nil, err
 	}
@@ -54,7 +51,7 @@ func (b *Backend) Update(ctx context.Context, wf *v1alpha1.Workflow) error {
 	return nil
 }
 
-func (b *Backend) ReadAllWorkflowRuleSets(ctx context.Context) ([]v1alpha1.WorkflowRuleSet, error) {
+func (b *Backend) ReadWorkflowRuleSets(ctx context.Context) ([]v1alpha1.WorkflowRuleSet, error) {
 	stored := &v1alpha1.WorkflowRuleSetList{}
 	err := b.cluster.GetClient().List(ctx, stored)
 	if err != nil {
@@ -65,7 +62,7 @@ func (b *Backend) ReadAllWorkflowRuleSets(ctx context.Context) ([]v1alpha1.Workf
 	return wfs, nil
 }
 
-func (b *Backend) Create(ctx context.Context, wf *v1alpha1.Workflow) error {
+func (b *Backend) CreateWorkflow(ctx context.Context, wf *v1alpha1.Workflow) error {
 	if err := b.cluster.GetClient().Create(ctx, wf); err != nil {
 		return fmt.Errorf("failed to create workflow %s: %w", wf.Name, err)
 	}
