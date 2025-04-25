7908308892d1474e0efea94d6fae229731a5ebdd
Jacob Weinstock <jakobweinstock@gmail.com>
2025-04-25 08:52:51 -0600

    Add normalization to GVR:
    This increases successful lookups
    by making sure GVR are in the correct
    format.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- pkg/backend/kube/controller.go
+++ pkg/backend/kube/controller.go
@@ -4,13 +4,22 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
+// DynamicRead reads any Kubernetes resource, defined via gvr, name, and namespace, and returns the spec field
+// as a map[string]interface{}. It uses the Kubernetes dynamic client to perform the read operation.
 func (b *Backend) DynamicRead(ctx context.Context, gvr schema.GroupVersionResource, name, namespace string) (map[string]interface{}, error) {
-	res := b.DynamicClient.Resource(gvr).Namespace(namespace)
+	// make sure all gvr field values are lowercase
+	sanitizedGVR := schema.GroupVersionResource{
+		Group:    strings.ToLower(gvr.Group),
+		Version:  strings.ToLower(gvr.Version),
+		Resource: strings.ToLower(gvr.Resource),
+	}
+	res := b.DynamicClient.Resource(sanitizedGVR).Namespace(namespace)
 	one, err := res.Get(ctx, name, metav1.GetOptions{})
 	if err != nil {
 		return nil, fmt.Errorf("error getting resource: %w", err)
