df914392d08728fd6c28f1fcf45bd80d890eb518
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2025-09-02 22:53:36 +0530

    fix a few typos
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

--- tink/server/internal/grpc/kube_test.go
+++ tink/server/internal/grpc/kube_test.go
@@ -80,7 +80,7 @@ func TestMakeValid(t *testing.T) {
 		"valid name with prefix and truncated name": {
 			inputName:    "very-long-name-that-exceeds-the-sixty-three-character-limit-and-needs-to-be-truncated",
 			inputPrefix:  "enrollment-",
-			expectedName: "enrollment-very-long-name-that-exceeds-the-sixty-three-characte",
+			expectedName: "enrollment-very-long-name-that-exceeds-the-sixty-three-character",
 		},
 		"valid name with prefix and empty name": {
 			inputName:     "",
