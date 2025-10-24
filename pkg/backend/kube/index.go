rpardini/tinkerbell-tinkerbell

tootles: introduce `/tootles/instanceID/:instanceID/2009-04-04` metadata prefix endpoint
- using Hardware CRD's .Metadata.Instance.ID, one can now get info out of tootles without needing a stable IP address
- in cloud-init.cfg: `metadata_urls: ["http://<tootlesHost>:50061/tootles/instanceID/<instanceId>"]`
- introduce `GetEC2InstanceByInstanceID()` member for Client interface
    - add indexed lookup via `.Spec.Metadata.Instance.ID` for kube backend's impl via `hwByInstanceID()`
    - no-op impl for noop backend
- frontend: extract method `writeInstanceDataOrErrToHTTP()`
  - call it from both IP-based and instanceId-based route groupings
- some maintenance tasks missing from Makefile:
    - ran gofumpt
      - `go install mvdan.cc/gofumpt@latest`
      - `gofumpt -l -w .`
    - updated tootles' frontend_mock_test.go
      - `go install go.uber.org/mock/mockgen@latest`
      - `cd tootles/internal/frontend/ec2`
      - `mockgen -source=frontend.go > frontend_mock_test.go`

Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

pkg/backend/kube/index.go
@@ -11,6 +11,7 @@ type IndexType string
 const (
 	IndexTypeMACAddr                    IndexType = MACAddrIndex
 	IndexTypeIPAddr                     IndexType = IPAddrIndex
+	IndexTypeInstanceID                 IndexType = InstanceIDIndex
 	IndexTypeWorkflowByNonTerminalState IndexType = WorkflowByNonTerminalState
 	IndexTypeHardwareName               IndexType = "hardware.metadata.name"
 	IndexTypeMachineName                IndexType = "machine.metadata.name"
@@ -42,6 +43,11 @@ var Indexes = map[IndexType]Index{
 		Obj:          &bmc.Machine{},
 		Field:        MachineNameIndex,
 		ExtractValue: MachineNameFunc,
+	},
+	IndexTypeInstanceID: {
+		Obj:          &v1alpha1.Hardware{},
+		Field:        InstanceIDIndex,
+		ExtractValue: InstanceIDFunc,
 	},
 }
 
@@ -90,6 +96,16 @@ func GetIPs(h *v1alpha1.Hardware) []string {
 		}
 	}
 	return ips
+}
+
+// InstanceIDIndex is an index used with a controller-runtime client to lookup hardware by its metadata instance id.
+const InstanceIDIndex = ".Spec.Metadata.Instance.ID" // #nosec G101
+func InstanceIDFunc(obj client.Object) []string {
+	hw, ok := obj.(*v1alpha1.Hardware)
+	if !ok {
+		return nil
+	}
+	return []string{hw.Spec.Metadata.Instance.ID}
 }
 
 // WorkflowByNonTerminalState is the index name for retrieving workflows in a non-terminal state.

tinkerbell/tinkerbell

pkg/backend/kube/index.go
@@ -15,6 +15,7 @@ const (
 	IndexTypeMachineName     IndexType = "machine.metadata.name"
 	IndexTypeWorkflowAgentID IndexType = WorkflowByAgentID
 	IndexTypeHardwareAgentID IndexType = HardwareByAgentID
+	IndexTypeInstanceID      IndexType = InstanceIDIndex
 )
 
 // Indexes that are currently known.
@@ -48,6 +49,11 @@ var Indexes = map[IndexType]Index{
 		Obj:          &tinkerbell.Hardware{},
 		Field:        HardwareByAgentID,
 		ExtractValue: HardwareByAgentIDFunc,
+	},
+	IndexTypeInstanceID: {
+		Obj:          &tinkerbell.Hardware{},
+		Field:        InstanceIDIndex,
+		ExtractValue: InstanceIDFunc,
 	},
 }
 
@@ -143,4 +149,14 @@ func HardwareByAgentIDFunc(obj client.Object) []string {
 		return []string{}
 	}
 	return []string{hw.Spec.AgentID}
+}
+
+// InstanceIDIndex is an index used with a controller-runtime client to lookup hardware by its metadata instance id.
+const InstanceIDIndex = ".Spec.Metadata.Instance.ID" // #nosec G101
+func InstanceIDFunc(obj client.Object) []string {
+	hw, ok := obj.(*tinkerbell.Hardware)
+	if !ok {
+		return nil
+	}
+	return []string{hw.Spec.Metadata.Instance.ID}
 }