02d1655a64f32ca0d76da821ca6b6d181508acfe
Jacob Weinstock <jakobweinstock@gmail.com>
2025-11-05 15:23:06 -0700

    Remove github.com/opencontainers/runtime-spec:
    This package has a critical security vulnerability,
    is only used for a single string type, and when
    upgraded conflicts with v1 of github.com/containerd/containerd.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- tink/agent/internal/runtime/containerd/containerd.go
+++ tink/agent/internal/runtime/containerd/containerd.go
@@ -12,7 +12,6 @@ import (
 	"github.com/containers/image/v5/pkg/shortnames"
 	"github.com/containers/image/v5/types"
 	"github.com/go-logr/logr"
-	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/tinkerbell/tinkerbell/tink/agent/internal/pkg/conv"
 	"github.com/tinkerbell/tinkerbell/tink/agent/internal/spec"
 )
@@ -94,7 +93,7 @@ func (c *Config) createContainer(ctx context.Context, image containerd.Image, ac
 		oci.WithProcessArgs(args...),
 	}
 	if action.Namespaces.PID == "host" {
-		specOpts = append(specOpts, oci.WithHostNamespace(specs.PIDNamespace))
+		specOpts = append(specOpts, oci.WithHostNamespace("pid"))
 	}
 	name := conv.ParseName(action.ID, action.Name)
 	newOpts = append(newOpts, containerd.WithNewSnapshot(name, image))
