c5b284cacd21139855ada498efb171dae721fc6f
Jacob Weinstock <jakobweinstock@gmail.com>
2025-12-19 10:32:04 -0700

    Update embedded CRD locations
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- crd/crd.go
+++ crd/crd.go
@@ -18,25 +18,25 @@ import (
 	"k8s.io/client-go/rest"
 )
 
-//go:embed bases/tinkerbell.org_hardware.yaml
+//go:embed bases/v1alpha1/tinkerbell.org_hardware.yaml
 var HardwareCRD []byte
 
-//go:embed bases/tinkerbell.org_templates.yaml
+//go:embed bases/v1alpha1/tinkerbell.org_templates.yaml
 var TemplateCRD []byte
 
-//go:embed bases/tinkerbell.org_workflows.yaml
+//go:embed bases/v1alpha1/tinkerbell.org_workflows.yaml
 var WorkflowCRD []byte
 
-//go:embed bases/tinkerbell.org_workflowrulesets.yaml
+//go:embed bases/v1alpha1/tinkerbell.org_workflowrulesets.yaml
 var WorkflowRuleSetCRD []byte
 
-//go:embed bases/bmc.tinkerbell.org_jobs.yaml
+//go:embed bases/v1alpha1/bmc.tinkerbell.org_jobs.yaml
 var JobCRD []byte
 
-//go:embed bases/bmc.tinkerbell.org_machines.yaml
+//go:embed bases/v1alpha1/bmc.tinkerbell.org_machines.yaml
 var MachineCRD []byte
 
-//go:embed bases/bmc.tinkerbell.org_tasks.yaml
+//go:embed bases/v1alpha1/bmc.tinkerbell.org_tasks.yaml
 var TaskCRD []byte
 
 // Tinkerbell is the struct that holds the raw custom resource definitions
