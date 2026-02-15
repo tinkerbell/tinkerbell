18b46a1c145500fb83e0bc838117e1dfa280bb9a
Jacob Weinstock <jakobweinstock@gmail.com>
2026-02-14 20:32:29 -0700

    Set the klog use the structured logger:
    There were some log lines that were not using
    the structured logger as all others do.
    This was coming from the client-go's leader
    election code.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- rufio/rufio.go
+++ rufio/rufio.go
@@ -24,6 +24,7 @@ import (
 	"github.com/go-logr/logr"
 	"github.com/tinkerbell/tinkerbell/rufio/internal/controller"
 	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2"
 	controllerruntime "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	clog "sigs.k8s.io/controller-runtime/pkg/log"
@@ -122,6 +123,7 @@ func (c *Config) Start(ctx context.Context, log logr.Logger) error {
 
 	controllerruntime.SetLogger(log)
 	clog.SetLogger(log)
+	klog.SetLogger(log)
 
 	mgr, err := controller.NewManager(c.Client, options, c.PowerCheckInterval, c.MaxConcurrentReconciles)
 	if err != nil {
