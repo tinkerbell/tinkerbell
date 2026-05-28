8be6f86d025bf39959a04fc280a711294dac7232
Jacob Weinstock <jakobweinstock@gmail.com>
2026-05-28 16:42:43 -0600

    Update k8s dep script and other deps:
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- script/deps.sh
+++ script/deps.sh
@@ -12,12 +12,15 @@ MODS=($(
     curl -sS https://raw.githubusercontent.com/kubernetes/kubernetes/v${VERSION}/go.mod |
     sed -n 's|.*k8s.io/\(.*\) => ./staging/src/k8s.io/.*|k8s.io/\1|p'
 ))
+GETS=()
 for MOD in "${MODS[@]}"; do
     V=$(
         go mod download -json "${MOD}@kubernetes-${VERSION}" |
         sed -n 's|.*"Version": "\(.*\)".*|\1|p'
     )
     go mod edit "-replace=${MOD}=${MOD}@${V}"
+    GETS+=("${MOD}@${V}")
 done
-go get "k8s.io/kubernetes@v${VERSION}"
+go get "k8s.io/kubernetes@v${VERSION}" "${GETS[@]}"
+go mod tidy
 
